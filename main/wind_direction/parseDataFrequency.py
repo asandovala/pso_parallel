7dc3c96851c9f9204a900ded95fc1768d60bfcd9
Alonso Sandoval <alonso.sandovala@alumnos.usm.cl>
2016-08-28 22:14:20 -0300

    start direction study

---
+++ main/wind_direction/parseDataFrequency.py
@@ -0,0 +1,61 @@
+import os
+import math as m
+
+PATH_RES = "../../data/2015/csv/"
+PATH_OUT = "../../data/2015/parse/"
+NAME_OUT = "direction_freq_data_15.csv"
+
+allDirections = []
+
+def getRelativeFreq():
+  dic = {}
+  total = 0.0
+  for direction in allDirections:
+    if direction in dic:
+      dic[direction] += 1
+      total += 1.0
+    else:
+      dic[direction] = 0 
+
+  for key in dic:
+    dic[key] = dic[key]/total     
+  print dic    
+
+def isNumber(s):
+  try:
+    int(s)
+    return True
+  except ValueError:
+    return False
+
+def getData(text):
+  for line in text[5:]:   #for each line
+    line = line.split(",")
+    if isNumber(line[1]):  #if more data
+      for i in range(2,18,2):
+      	if (float(line[i + 1]) > 0):
+      	 allDirections.append( m.pi * (float(line[i])) * 0.0055555 )
+    else:  #No more data
+      return
+
+def saveFreqYear():
+  with open(PATH_OUT + NAME_OUT, "w") as f:
+    for i in range(0, len(allDirections)):
+      data = allDirections[i]		
+      line = str(data)
+      if i < len(allDirections) - 1: 
+      	f.write(line + '\n')
+      else: 
+      	f.write(line)	
+
+for fileName in os.listdir(PATH_RES):
+  with open(PATH_RES + fileName, "r") as f:
+    print fileName
+
+    text = f.read()
+    text = text.split("\n")
+    getData(text)
+
+saveFreqYear()
+
+#getRelativeFreq()
---
+++ main/wind_direction/plotDirFreq.py
@@ -0,0 +1,34 @@
+import os
+import math as m
+import numpy as np
+import matplotlib.pyplot as plt
+
+PATH_DATA = "../../data/2015/parse/direction_freq_data_15.csv"
+
+bisDirection = []
+
+def initBins():
+    global bisDirection
+    for i in range(1, 29):
+        bisDirection.append(0.07 * i) #each 12.6 degrees app
+
+with open(PATH_DATA, "r") as f:
+    text = f.read()
+    text = text.split("\n")
+    allData = []
+    for i in range(0, len(text)):
+        allData.append(float(text[i])/m.pi)
+       
+
+#plot
+initBins()
+
+weights = [100.0/len(allData) for i in range(0, len(allData))]
+plt.hist(allData, bisDirection, align = 'mid',  weights = weights, histtype='bar', rwidth=1)
+
+#plt.axis([0, 15, 0, 50])
+plt.legend(bbox_to_anchor=(0.6, 0.8), loc=2, borderaxespad=0)
+plt.xlabel("Direccion [rad]")
+plt.ylabel("Frecuencia (%)")
+plt.title(r"""$Direcci\acute{\o}n\ del\ viento\ 2015\ Valpara\acute{\imath}so$""")
+plt.show()
